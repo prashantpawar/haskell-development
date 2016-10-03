9669a8583bc2cd3dc499c8df6851b204c4dc87d9
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-10-02 21:56:56 -0400

    Initial commit

---
+++ count-luck/app/Main.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import System.IO
+import Data.List
+import Debug.Trace
+
+main :: IO ()
+main = do
+    t_temp <- getLine
+    let t = read $ t_temp :: Int
+    output_temp <- solve t
+    let output = output_temp
+    print $ show output
+
+solve :: Int -> IO [String]
+solve t
+    | t <= 0 = return []
+    | otherwise = do
+        n_m_temp <- getLine
+        let n_m = words n_m_temp
+        let n = trace(show n_m)(n_m)
+        -- let m = read n_m!!1 :: Int
+        matrix_temp <- getMultipleLines 5
+        let matrix = matrix_temp
+        return matrix
+
+getMultipleLines :: Int -> IO [String]
+getMultipleLines n
+    | n <= 0 = return []
+    | otherwise = do
+        x <- getLine
+        xs <- getMultipleLines (n-1)
+        let ret = (x:xs)
+        return ret
